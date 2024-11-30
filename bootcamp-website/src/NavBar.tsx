9b164e3b15d0dad1070616650b9428bf6bf637b7
Milana_Dagne <mdagne12@terpmail.umd.edu>
2024-11-30 12:10:01 -0500

    added background

---
+++ bootcamp-website/src/NavBar.tsx
@@ -0,0 +1,11 @@
+import './NavBar.css'
+
+function NavBar() {
+    return (
+        <div className='container'>
+            <h2 className='app-dev-title'> App Dev Bootcamp </h2>
+        </div>
+    )
+}
+
+export default NavBar
---
+++ bootcamp-website/src/background.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const BackgroundImage: React.FC = () => {
+  return (
+    <div className="background">
+      <h1>Welcome to My Website</h1>
+    </div>
+  );
+};
+
+export default BackgroundImage;
